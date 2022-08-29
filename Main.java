039f6b6fb0a15ce310e0d2e9cdc4d9142d72a737
Song Vu Nguyen <vunnems08@gmail.com>
2022-08-29 13:44:42 -0400

    Initialize repo

---
+++ Main.java
@@ -0,0 +1,8 @@
+/* Main functions to run test for all algorithms classes */
+
+
+public class Main{
+    public static void main (String[] args){
+        System.out.println("This is a test");
+    }
+}
